bb6f969c15d5abc291311a3f232d3a9e97b7f904
ikifirdaus <ikifirdaus@gmail.com>
2025-05-30 00:08:35 +0700

    modifyImageLanding

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="bg-[#C26728] flex justify-center items-center min-h-screen">
-      <Image src="/logo6.png" width={400} height={100} alt="logo" />
+      <Image
+        src="/logo6.png"
+        width={400}
+        height={100}
+        alt="logo"
+        className="w-80 md:w-auto"
+      />
     </div>
   );
 }
